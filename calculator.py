fe30ff6a4d8a6e38116bf4c6eee2b370722ff279
ratindersingh <ratinder021@gmail.com>
2024-04-25 08:40:28 +0530

    Add initial calculator script

---
+++ calculator.py
@@ -0,0 +1,12 @@
+# Test the functions
+result = add(5, 3)
+print("5 + 3 =", result)
+
+result = subtract(7, 2)
+print("7 - 2 =", result)
+
+result = multiply(4, 6)
+print("4 * 6 =", result)
+
+result = divide(8, 2)
+print("8 / 2 =", result)
